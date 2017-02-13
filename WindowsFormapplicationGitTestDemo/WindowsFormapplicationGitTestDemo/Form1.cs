83ef446dc9efa1e02acd414d2368b0d881cbda6e
avirajlole <avirajlole@gmail.com>
2017-02-13 13:18:41 +0530

    Messagebox Commit

--- WindowsFormapplicationGitTestDemo/WindowsFormapplicationGitTestDemo/Form1.cs
+++ WindowsFormapplicationGitTestDemo/WindowsFormapplicationGitTestDemo/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormapplicationGitTestDemo
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello..");
+        }
     }
 }
